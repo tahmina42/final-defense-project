6751860f9f07ee4bb7a128f1ced1b03f3ddeb825
tahmina42 <tahminamukta42@gmail.com>
2020-09-30 22:20:19 +0600

    new changes added after 80%

--- mail.php
+++ mail.php
@@ -11,7 +11,7 @@ if (isset($_GET['id'])){
 	
 	$result = $mailing->select("SELECT * FROM vaccine_assign WHERE p_id=$id");
 	
-	
+	//need to relate database
 	foreach ($result as $res) {
 		
 		$user_name 	=($res['p_name']);
